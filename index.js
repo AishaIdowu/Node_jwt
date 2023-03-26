60740468ac4659cea7dfd400c7ac2fc8102a9fc1
AishaIdowu <idowueniola@domain.com>
2023-03-26 18:31:57 +0100

    Token added

--- index.js
+++ index.js
@@ -8,14 +8,27 @@ app.get("/api", (req, res) => {
   });
 });
 
-app.post("/api/posts", verifyToken,  (req, res) => {
+app.post("/api/posts", verifyToken, (req, res) => {
+
+  jwt.verify(req.token, 'secretKey', (error, authData) => {
+    if(error) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        message: 'Post created....',
+
+        authData
+      })
+    }
+  })
+
   res.json({
     message: "Post created...",
   });
 });
 
 // Token
-app.post("/api/login",(req, res) => {
+app.post("/api/login", (req, res) => {
   // Mock user
   const user = {
     id: 1,
@@ -23,28 +36,34 @@ app.post("/api/login",(req, res) => {
     email: "aisha@gmail.com",
   };
 
-  jwt.sign({ user: user }, "secretKey", (error, token) => {
+  jwt.sign({ user: user }, "secretKey",{expiresIn:'60s'}, (error, token) => {
     res.json({
       token,
     });
   });
 });
 
-// FORMAT OF TOKEN 
+// FORMAT OF TOKEN
 // Authorzation: Bearer <access_token>
 
 // Verify Token
 function verifyToken(req, res, next) {
-    // Get auth header value
-    const bearerHeader = req.headers['authorization'];
-    // Check if bearer is udefined
-    if(typeof bearerHeader !== 'undefined') {
-
-    } else {
-        // forbidden
-        res.status(403);
-    }
-    
+  // Get auth header value
+  const bearerHeader = req.headers["authorization"];
+  // Check if bearer is udefined
+  if (typeof bearerHeader !== "undefined") {
+    // split at the space
+    const bearer = bearerHeader.split(" ");
+    // Get token from array
+    const bearerToken = bearer[1];
+    // Set the token
+    req.token = bearerToken;
+    // Next Middleware
+    next();
+  } else {
+    // forbidden
+    res.sendStatus(403);
+  }
 }
 
 const PORT = process.env.PORT || 3000;
